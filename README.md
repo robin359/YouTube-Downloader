cdb668ff372f716e5f827b72b6027c3fe1545a08
robin359 <robinwahl1997@gmail.com>
2023-04-19 15:31:30 +0200

    added tkinter GUI

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
 # YouTube-Downloader
 
-This little script helps you downloading single videos and whole playlists from YouTube. 
-Currently it is a CLI tool, but I am working on making a GUI for it. 
+This little app helps you downloading single videos and whole playlists from YouTube. 
 
-## Usage
-Download the script. Open up a terminal and execute the script with python3:
-
-`python3 youtubeDownloader.py`
-
-Then enter the `URL`, type `mp3` for audio or `mp4` for video file and enter a destination directory (optional)
-
-Voilá your file is downloaded!
+## Now with tkinter GUI
--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,5 +1,8 @@
 from pytube import YouTube, Playlist
 import os
+import tkinter as tk
+from tkinter import filedialog
+
 
 def download_video(video, format, destination):
     if format == "mp3":
@@ -21,29 +24,66 @@ def download_video(video, format, destination):
 
     print("Download completed.")
 
+
 def download():
-    # Input URL from User
-    url = input("Enter URL: ")
-    # Check if input URL is a playlist
-    if 'playlist' in url.lower():
-        playlist = Playlist(url)
-        # Ask if User wants mp3 or mp4
-        format = input("Download mp3 or mp4? ")
-        # Enter destination directory
-        destination = str(input("Enter the destination (leave blank for current directory)\n >> ")) or '.'
+    # Create GUI window
+    root = tk.Tk()
+    root.title("YouTube Downloader")
 
-        for video in playlist.videos:
-            download_video(video, format, destination)
+    # Create input URL entry
+    url_label = tk.Label(root, text="Enter URL:")
+    url_label.grid(row=0, column=0)
+    url_entry = tk.Entry(root, width=50)
+    url_entry.grid(row=0, column=1)
 
-    else:
-        # If input URL is not a playlist, download the single video
-        yt = YouTube(url)
-        # Ask if User wants mp3 or mp4
-        format = input("Download mp3 or mp4? ")
-        # Enter destination directory
-        destination = str(input("Enter the destination (leave blank for current directory)\n >> ")) or '.'
-        download_video(yt, format, destination)
+    # Create format dropdown
+    format_label = tk.Label(root, text="Download format:")
+    format_label.grid(row=1, column=0)
+    format_var = tk.StringVar(value="mp4")
+    format_dropdown = tk.OptionMenu(root, format_var, "mp3", "mp4")
+    format_dropdown.grid(row=1, column=1)
 
+    # Create destination directory entry
+    destination_label = tk.Label(root, text="Destination directory:")
+    destination_label.grid(row=2, column=0)
+    destination_var = tk.StringVar(value=os.getcwd())
+    destination_entry = tk.Entry(root, textvariable=destination_var, width=50)
+    destination_entry.grid(row=2, column=1)
+    def choose_destination():
+        chosen_directory = filedialog.askdirectory()
+        if chosen_directory:
+            destination_var.set(chosen_directory)
+    choose_destination_button = tk.Button(root, text="Choose directory", command=choose_destination)
+    choose_destination_button.grid(row=2, column=2)
+
+    def start_download():
+        url = url_entry.get()
+        # Check if input URL is a playlist
+        if 'playlist' in url.lower():
+            playlist = Playlist(url)
+            # Ask if User wants mp3 or mp4
+            format = format_var.get()
+            # Enter destination directory
+            destination = destination_var.get()
+
+            for video in playlist.videos:
+                download_video(video, format, destination)
+
+        else:
+            # If input URL is not a playlist, download the single video
+            yt = YouTube(url)
+            # Ask if User wants mp3 or mp4
+            format = format_var.get()
+            # Enter destination directory
+            destination = destination_var.get()
+            download_video(yt, format, destination)
+
+    # Create download button
+    download_button = tk.Button(root, text="Download", command=start_download)
+    download_button.grid(row=3, column=1)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
     download()
-
-download()
