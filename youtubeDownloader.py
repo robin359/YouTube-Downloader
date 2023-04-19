5f4262cc0b25b186239f7e788e512d3f58fdb587
robin359 <robinwahl1997@gmail.com>
2023-04-19 15:02:00 +0200

    Made it more modular

--- youtubeDownloader.py
+++ youtubeDownloader.py
@@ -1,12 +1,29 @@
 from pytube import YouTube, Playlist
 import os
 
-# Download Function
+def download_video(video, format, destination):
+    if format == "mp3":
+        ys = video.streams.filter(only_audio=True).first()
+    elif format == "mp4":
+        ys = video.streams.get_highest_resolution()
+    else:
+        return 0
+
+    # Downloading file
+    print("Downloading...")
+    out_file = ys.download(output_path=destination)
+
+    # Changing file extension if user chose mp3
+    if format == "mp3":
+        base, ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+
+    print("Download completed.")
+
 def download():
-    
     # Input URL from User
     url = input("Enter URL: ")
-    
     # Check if input URL is a playlist
     if 'playlist' in url.lower():
         playlist = Playlist(url)
@@ -14,28 +31,10 @@ def download():
         format = input("Download mp3 or mp4? ")
         # Enter destination directory
         destination = str(input("Enter the destination (leave blank for current directory)\n >> ")) or '.'
-        
+
         for video in playlist.videos:
-            # Logic for downloading mp3 or mp4
-            if format == "mp3":
-                ys = video.streams.filter(only_audio=True).first()
-            elif format == "mp4":
-                ys = video.streams.get_highest_resolution()
-            else:
-                return 0
-            
-            # Downloading file
-            print("Downloading...")
-            out_file = ys.download(output_path=destination)
-            
-            # Changing file extension if user chose mp3
-            if format == "mp3":
-                base, ext = os.path.splitext(out_file)
-                new_file = base + '.mp3'
-                os.rename(out_file, new_file)
-            
-            print("Download completed.")
-        
+            download_video(video, format, destination)
+
     else:
         # If input URL is not a playlist, download the single video
         yt = YouTube(url)
@@ -43,28 +42,8 @@ def download():
         format = input("Download mp3 or mp4? ")
         # Enter destination directory
         destination = str(input("Enter the destination (leave blank for current directory)\n >> ")) or '.'
-        
-        # Logic for downloading mp3 or mp4
-        if format == "mp3":
-            ys = yt.streams.filter(only_audio=True).first()
-        elif format == "mp4":
-            ys = yt.streams.get_highest_resolution()
-        else:
-            return 0
-        
-        # Downloading file
-        print("Downloading...")
-        out_file = ys.download(output_path=destination)
-        
-        # Changing file extension if user chose mp3
-        if format == "mp3":
-            base, ext = os.path.splitext(out_file)
-            new_file = base + '.mp3'
-            os.rename(out_file, new_file)
-        
-        print("Download completed.")
-    
-    # Continuing
+        download_video(yt, format, destination)
+
     download()
 
 download()
